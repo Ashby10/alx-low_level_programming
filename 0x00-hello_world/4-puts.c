9b5be4150fd60d5011b10f3f26ac564b5738c580
Achraf <achraf.rifqi@gmail.com>
2023-06-16 01:26:21 +0100

    4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ * this is a puts function file
+ *main : entry point
+ *return : 0
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
