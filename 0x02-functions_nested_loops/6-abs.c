b6428e2a5c02d03f23c377bce30759e508ff8d85
Achraf <achraf.rifqi@gmail.com>
2023-06-21 01:21:20 +0100

    3

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs -  absolute value of int
+ * @n: check
+ * Return: the absolute value of int
+ */
+int _abs(int n)
+{
+if (n >= 0)
+{
+return (n);
+}
+return (-n);
+}
