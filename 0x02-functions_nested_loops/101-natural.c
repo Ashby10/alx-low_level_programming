357beb0d8e82752418bd16b467bc67cd4caef9c4
Achraf <achraf.rifqi@gmail.com>
2023-06-23 02:09:37 +0100

    100

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,26 @@
+/*
+ * File: 101-natural.c
+ * Auth: achraf
+ */
+
+#include <stdio.h>
+
+/**
+ * main - Lists natural numbers < 1024
+ *        that are multiples of 3 or 5.
+ *
+ * Return:  0
+ */
+int main(void)
+{
+int i, sum = 0;
+for (i = 0; i < 1024; i++)
+{
+if ((i % 3) == 0 || (i % 5) == 0)
+sum += i;
+}
+
+printf("%d\n", sum);
+
+return (0);
+}
