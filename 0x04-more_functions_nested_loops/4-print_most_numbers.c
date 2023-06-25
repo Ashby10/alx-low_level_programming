abd5b8101fa75170c4e0a3d209dc237a643c47af
Achraf <achraf.rifqi@gmail.com>
2023-06-25 01:30:42 +0100

    0

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -2,16 +2,18 @@
 
 /**
  * print_numbers - prints most numbers
+ *
  * Retrun: void
  */
 
 void print_most_numbers(void)
 {
 char i;
+
 for (i = '0'; i <= '9'; i++)
 {
-if ((i != '2') && (i != '4'))
-_putchar(i);
+	if ((i != '2') || (i != '4'))
+	_putchar(i);
 }
 	_putchar('\n');
 }
