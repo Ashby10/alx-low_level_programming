8b41555b0823fad9ee5703fef6c2a5c546d435db
ashby10 <achraf.rifqi@gmail.com>
2023-06-26 15:42:55 -0700

    7

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,17 +10,22 @@ int i;
 
 for(i = 1; i <= 100; i++)
 {
+if (i == 1)
+	printf("%d, i");
+else
+{
 if (((i % 3) != 0) && ((i % 5 != 0)))
-	printf("%d", i);
+	printf(" %d", i);
 else
 	{
 if (((i % 3) == 0) && ((i % 5 != 0)))
-		printf("Fizz");
+		printf(" Fizz");
 if (((i % 3) != 0) && ((i % 5 == 0)))
-		printf("Buzz");
+		printf(" Buzz");
 if (((i % 3) == 0) && ((i % 5 == 0)))
-		printf("FizzBuzz");
+		printf(" FizzBuzz");
 	}
+}
 }
 printf("\n");
 return (0);
