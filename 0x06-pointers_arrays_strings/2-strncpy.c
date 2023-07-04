734a2ffbf733ea0720ef6166deb747a7dc811e5a
Achraf <achraf.rifqi@gmail.com>
2023-07-05 00:59:23 +0100

    0x06

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -20,9 +20,9 @@ char *_strncpy(char *dest, char *src, int n)
 		j++;
 	}
 	
-	while (j < n && dest[j] != '\0')
+	while (j < n)
 	{		
-		dest[j] = ' ';
+		dest[j] = '\0';
 		j++;
 	}
 return (dest);
