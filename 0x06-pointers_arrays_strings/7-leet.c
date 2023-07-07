f3a212fd1a81e30ffe7b46e88c7cfeccf1b72632
Achraf <achraf.rifqi@gmail.com>
2023-07-07 20:41:35 +0100

    0x06

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * leet - a function that replaces chars
+ * @s: pointer to a string
+ *
+ * Return: string pointer
+ */
+char *string_toupper(char *s)
+{
+	int i;
+	char a[10] = {"aAeEoOtTlL"};
+	char b[10] = {"4433007711"};
+	i = 0;
+	while (a[i] != '\0')
+	{
+		while (i < j)
+		{
+			if (s[i] == a[i])
+			{
+				a[i] = a[i] - 32;
+				j++;
+			}
+		}
+		i++;
+	}
+	return (s);
+}
