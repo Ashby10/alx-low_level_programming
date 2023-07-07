fe70eb3aafd2ef2079093a4c05d8b52de835d932
Achraf <achraf.rifqi@gmail.com>
2023-07-07 11:47:48 +0100

    0x06

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * reverse_array - a function that reverse an array
+ * @a: pointer to an int value
+ * @n: int
+ *
+ * Return: void
+ */
+void reverse_array(int *a, int n)
+{
+	int i;
+	for (i = 0; i < n--; i++)
+	{
+		c = a[i];
+		a[i] = a[j];
+		a[j] = c;
+	}
+}
