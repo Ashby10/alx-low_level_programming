ff160e963939c7faa8ad1e630cdab35bdf54764f
Achraf <achraf.rifqi@gmail.com>
2023-07-07 11:30:10 +0100

    0x06

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * _stcmp - a function that compares 2 strings
  * @s1: pointer of a char
@@ -18,12 +17,12 @@ int _strcmp(char *s1, char *s2)
 	t2 = 0;
 	while (src[j] != '\0')
 	{
-		t1 += s1[j];
+		t1 = t1 + s1[j];
 		j++;
 	}
 	while (s2[i] != '\0')
 	{
-		t2 += s2[i];
+		t2 = t2 + s2[i];
 		i++;
 	}
 return (t1 - t2);
