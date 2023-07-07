5ac2ee694a85370caa573d00a922f1a508e0029e
Achraf <achraf.rifqi@gmail.com>
2023-07-07 12:02:52 +0100

    0x06

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -10,11 +10,11 @@ char *string_toupper(char *a)
 	int i;
 
 	i = 0;
-	while (a[i] != '\0')
+	while(a[i] != '\0')
 	{
 		if (a[i] >= 'a' && a[i] <= 'z')
 			a[i] = a[i] - 32;
 		i++;
 	}
-	return a;
+	return (a);
 }
