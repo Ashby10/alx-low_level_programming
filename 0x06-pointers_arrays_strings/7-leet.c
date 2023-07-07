4ddd57a212ddd09f0e6e4b8eab3c571f4c65d735
Achraf <achraf.rifqi@gmail.com>
2023-07-07 23:44:45 +0100

    0x06

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -14,16 +14,13 @@ char *leet(char *s)
 
 	i = 0;
 	j = 0;
-	while (s[i] != '\0')
+	for (i = 0; s[i] != '\0'; i++)
 	{
-	for (j = 0; j < 10; j++)
+		for (j = 0; j < 10; j++)
 		{
 			if (s[i] == a[j])
-			{
-				s[i] = b[j];
-			}
+			s[i] = b[j];
 		}
-		i++;
 	}
 	return (s);
 }
