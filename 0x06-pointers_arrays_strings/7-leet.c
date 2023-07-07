5db04301bb7d3eee72ca46843d306d0acfa2b50a
Achraf <achraf.rifqi@gmail.com>
2023-07-07 20:43:23 +0100

    0x06

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -8,8 +8,8 @@
 char *string_toupper(char *s)
 {
 	int i;
-	char a[10] = {"aAeEoOtTlL"};
-	char b[10] = {"4433007711"};
+	char a[10] = "aAeEoOtTlL";
+	char b[10] = "4433007711";
 	i = 0;
 	while (a[i] != '\0')
 	{
@@ -17,7 +17,7 @@ char *string_toupper(char *s)
 		{
 			if (s[i] == a[i])
 			{
-				a[i] = a[i] - 32;
+				s[i] = a[j];
 				j++;
 			}
 		}
