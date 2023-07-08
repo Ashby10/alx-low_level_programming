c2c3afab860c882b893bf1143cae707570961845
Achraf <achraf.rifqi@gmail.com>
2023-07-08 14:52:07 +0100

    0x08

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * _puts_recursion - a recursive funtion puts
+ * @s: string
+ * Retrun: void
+ */
+void _puts_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	putchar('\n');
+}
