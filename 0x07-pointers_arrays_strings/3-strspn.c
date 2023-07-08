f1087d747faf88ab74e01298e86f52416f9b25d4
Achraf <achraf.rifqi@gmail.com>
2023-07-08 01:24:28 +0100

    0x07

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * _strspn - function
+ * @s: string pointer
+ * @accept: string pointer
+ * Return: unsigned integer
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	int i;
+	int j;
+	unsigned int t;
+
+	t = 0;
+	for (i = 0; s[i] != '\0'; i++)
+	{	
+		for (j = 0; accept[j] != '\0'; j++)
+			if (s[i] == accept[j])
+			{
+				t++;
+				break;
+			}
+			else if (accept[j + 1] == '\0')
+					return (t);
+	}
+	return (t);
+}
