9c9846df81ce37396a1806ffc1086d40793f1e4e
ashby10 <achraf.rifqi@gmail.com>
2023-07-12 11:23:02 -0700

    B

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <stdlib.h>
 /**
- * _strdup - a function that creates a pointer nd allocate memory
+ * _strdup - a function that duplicate a string
  * @str: string to copy
  * Description: allocate memory for an array
  * Return: pointer or NULL
@@ -12,12 +12,14 @@ char *ptr;
 unsigned int i;
 unsigned int j;
 
+if (str == 0)
+	return (NULL);
 j = 0;
 i = 0;
 while (str[i] != '\0')
 i++;
 ptr = malloc(sizeof(char) * (i + 1));
-if (ptr == NULL || i == 0)
+if (ptr == NULL)
 return (NULL);
 
 for (j = 0; j < i; j++)
