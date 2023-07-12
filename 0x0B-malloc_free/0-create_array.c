5e638763a9dddd1ed2911b46199616a5c950d9aa
ashby10 <achraf.rifqi@gmail.com>
2023-07-12 09:39:10 -0700

    B

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -14,7 +14,7 @@ int unsigned i;
 
 if (size == 0)
 return (NULL);
-ptr = malloc(sizeof(char) * size);
+ptr = malloc (sizeof(char) * size);
 if (ptr == NULL)
 return (NULL);
 for (i = 0; i < size; i++)
