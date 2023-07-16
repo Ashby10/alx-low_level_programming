febca0c5d377210d58152d6b9643dff3d7a80fc0
Achraf <achraf.rifqi@gmail.com>
2023-07-16 20:09:45 +0100

    0x0C

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -11,13 +11,15 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 char *ptr;
-if (size == 0 || nmeb == 0)
+unsigned int i;
+if (size == 0 || nmemb == 0)
 	return (NULL);
 else
-ptr = malloc (nmeb * size);
+ptr = malloc (nmemb * size);
 if (!ptr)
 return (NULL);
 else
-	for (i = 0; i < nmeb; i++)
+	for (i = 0; i < nmemb; i++)
 		ptr[i] = 0;
+return (ptr);
 }
