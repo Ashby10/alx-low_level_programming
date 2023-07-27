8091bfea076f4d41a6ac4288d2b9ee7b5e95228d
Achraf <achraf.rifqi@gmail.com>
2023-07-27 23:09:11 +0100

    12

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+ *
+ *
+ *
+ *
+ *
+ */
+size_t list_len(const list_t *h)
+{
+list_t *tr;
+size_t i;
+
+i = 0;
+tr = head;
+while (tr->*next != NULL)
+{
+	if (tr->str == NULL)
+		print ("[0] (nil)\n");
+	else
+		printf("[%u] %s\n", tr->len, tr->str);
+tr = tr->next;
+i++;
+}
+return(i);
+}
