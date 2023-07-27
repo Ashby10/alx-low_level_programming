7846b00528003af787f266572be533d2d53e91bb
Achraf <achraf.rifqi@gmail.com>
2023-07-27 23:23:18 +0100

    0x12

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,13 +1,12 @@
 #include "lists.h"
 #include <stdio.h>
 /**
+ * print_list - function that prints a list
+ * @h: pointer head
  *
- *
- *
- *
- *
+ * Return: number of nodes
  */
-size_t list_len(const list_t *h)
+size_t print_list(const list_t *h)
 {
 list_t *tr;
 size_t i;
