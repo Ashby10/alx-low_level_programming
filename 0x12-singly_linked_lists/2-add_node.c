0de939848f3440ffe6739e8a17ec5f80295d6183
Achraf <achraf.rifqi@gmail.com>
2023-07-28 00:19:35 +0100

    0x12

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,9 +1,12 @@
 #include "lists.h"
+#include <string.h>
+#include <stdlib.h>
 /**
+ * add_node - function that adds a node
+ * @head: pointer to a pointer
+ * @str: string
  *
- *
- *
- *
+ * Return: the new node
  */
 list_t *add_node(list_t **head, const char *str)
 {
