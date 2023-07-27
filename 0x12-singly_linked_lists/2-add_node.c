da2ec7d1fe43072f9c48bd021c582c85e1ef395a
Achraf <achraf.rifqi@gmail.com>
2023-07-28 00:13:33 +0100

    0x12

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+/**
+ *
+ *
+ *
+ *
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	unsigned int l;
+	list_ t *new;
+
+new = malloc(sizeof(list_t));
+if (!new)
+	return (NULL);
+	l = 0;
+while (str[l])
+	l++;
+new->str = strdup(str);
+new->len = l;
+new->next = *head;
+*head-next = new;
+return (*head);
+}
