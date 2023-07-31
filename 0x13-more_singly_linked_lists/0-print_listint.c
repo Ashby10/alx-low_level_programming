b89f00a441dc446ad91326d50eff781ed62b9e76
Achraf <achraf.rifqi@gmail.com>
2023-07-31 12:05:08 +0100

    0x12

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,21 @@
+#include "lists_h"
+#include <stdio.h>
+/*
+ *
+ *
+ *
+ *
+ *
+ */
+size_t print_listint (const listint_t *h)
+{
+	size_t c = 0;
+
+	while (h)
+	{
+	printf("%d\n", h->n);
+	c++;
+	h = h->next;
+	}
+return (c);
+}
