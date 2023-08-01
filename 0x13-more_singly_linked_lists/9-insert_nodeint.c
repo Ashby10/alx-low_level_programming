65810f64cdd9c8b4ce12cdfda2083143c9e6258d
Achraf <achraf.rifqi@gmail.com>
2023-08-01 23:36:24 +0100

    0x12

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,4 +1,3 @@
-#include <stdlib>
 #include "lists.h"
 /**
  * insert_nodeint_at_index - inserts a new node nth position
@@ -11,7 +10,7 @@
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 unsigned int i;
-listint_t *new, *t, *tm;
+listint_t *new, *t;
 
 t = *head;
 i = 0;
