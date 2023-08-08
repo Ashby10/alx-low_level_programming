6e47e90bc1eb610232757ad2bba04fe03cda8196
Achraf <achraf.rifqi@gmail.com>
2023-08-08 01:51:38 +0100

    0x12

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * read_textfile- Read a file nad print it.
+ * @filename: file name
+ * @letters: number of letters
+ * Return: w- number of bytes read and printed or NULL.
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	char *buff;
+	ssize_t fd;
+	ssize_t w;
+	ssize_t t;
+
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+	buf = malloc(sizeof(char) * letters);
+	t = read(fd, buff, letters);
+	w = write(STDOUT_FILENO, buf, t);
+
+	free(buff);
+	close(fd);
+	return (w);
+}
